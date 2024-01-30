72dc39f59eabd4c29049b12b2c5738cf8b45f775
Franco Silva <franco_tigres18@hotmail.com>
2024-01-30 16:41:36 -0600

    feat(RegisterController):guardar registro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -17,13 +20,36 @@ class RegisterController extends Controller
         // dd($request);
         // dd($request->get('name'));
 
+        //* Modificar el request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         // * Validación
         $this->validate($request, [
             // 'name' => ['required', 'max:30']
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            // 'password' => $request->password,
+        ]);
+
+        //! Autenticar un usuario
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password,
+        // ]);
+
+        //! Forma 2 autenticar
+        auth()->attempt($request->only('email', 'password'));
+
+        // Redireccionar
+        return redirect()->route('posts.index');
     }
 }

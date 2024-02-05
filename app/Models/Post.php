f0064241061d35cf6acff73d95cd52d05aa34fe8
Franco Silva <franco_tigres18@hotmail.com>
2024-02-05 15:14:12 -0600

    feat(Likes a posts)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,18 +9,30 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable =[
+    protected $fillable = [
         'titulo',
         'descripcion',
         'imagen',
         'user_id',
     ];
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class)->select(['name', 'username']);
     }
 
-    public function comentarios() {
+    public function comentarios()
+    {
         return $this->hasMany(Comentario::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function checkLike(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
     }
 }

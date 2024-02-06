b6686373a82277a5fb9e04e3663b7674793f8269
Franco Silva <franco_tigres18@hotmail.com>
2024-02-06 10:27:34 -0600

    feat(Seguir,Dejar de seguir usuarios)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\FollowerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
@@ -50,3 +51,7 @@ Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.sto
 // Like a la fotos
 Route::post('/post/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/post/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+// Siguiendo usuarios
+Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');

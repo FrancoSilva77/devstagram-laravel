266e2ce197d289535d4b8bb9e9c50c94aaf62f2b
Franco Silva <franco_tigres18@hotmail.com>
2024-02-05 22:56:15 -0600

    feat(Editar perfil)

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -12,15 +12,10 @@ class ImagenController extends Controller
     public function store(Request $request)
     {
         $imagen = $request->file('file');
-
         $nombreImagen = Str::uuid() . "." . $imagen->extension(); 
-
         $imagenServidor = Image::make($imagen);
-
         $imagenServidor->fit(1000, 1000);
-
         $imagenPath = public_path('uploads') . '/' . $nombreImagen;
-
         $imagenServidor->save($imagenPath);
 
         return response()->json(['imagen' => $nombreImagen]);
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -30,6 +31,10 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('logout', [LogoutController::class, 'store'])->name('logout');
+
+// Rutas para el perfil
+Route::get('/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');

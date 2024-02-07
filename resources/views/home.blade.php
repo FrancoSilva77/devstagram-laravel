090e2c56c04e84ac0894e44ce6611cb313dbef08
Franco Silva <franco_tigres18@hotmail.com>
2024-02-06 19:37:51 -0600

    feat(Pagina principal, ver publicaciones)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('titulo')
-Página Principal
+    Página Principal
 @endsection
 
 @section('contenido')
-    Contenido de esta página
+    <x-listar-post :posts="$posts" />
 @endsection
